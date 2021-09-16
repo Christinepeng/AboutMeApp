5607bd18afd611bc7a763ade94f7628acb015b42
Christinepeng <percussionpeng@example.com>
2021-09-16 16:19:11 -0700

    update layout

--- app/src/main/java/com/example/abouyme/MainActivity.kt
+++ app/src/main/java/com/example/abouyme/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.abouyme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.view.View as View1
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
     }
+
+    private fun addNickname(view: View1) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View1.GONE
+        view.visibility = View1.GONE
+        nicknameTextView.visibility = View1.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
